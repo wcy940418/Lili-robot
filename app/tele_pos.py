358d4c196b0808840238d9b98300bd9fe2cafa78
Caleb <caleb.ho@me.com>
2016-06-03 10:30:32 -0400

    write create_pose_msg

--- app/tele_pos.py
+++ app/tele_pos.py
@@ -42,11 +42,20 @@ def init_socket():
 			print "Trying again in %d seconds" % t
 			time.sleep(t)
 
-def create_pose_msg(d, m):
-	"""Given coordinate data `d` and a pose message `m`, set the values of `m`
-	corresponding to the fields in `d`.
+def create_pose_msg(d, seq, m):
+	"""Given coordinate data `d`, number of iterations `seq`, and a pose message
+	`m`, set the values of `m` corresponding to the fields in `d`.
 	"""
-	pass
+	dic = rpy2quaternion(0, 0, d['yaw'])
+	m.header.seq = seq
+	m.header.stamp = rp.Time.now() - rp.Duration(5)
+	m.pose.position.x = d['x']
+	m.pose.position.y = d['y']
+	m.pose.position.z = d['z']
+	m.pose.orientation.x = 0.0
+	m.pose.orientation.y = 0.0
+	m.pose.orientation.z = dic['z']
+	m.pose.orientation.w = dic['w']
 
 def read_recent_pose():
 	"""
@@ -66,6 +75,8 @@ def main():
 	                          GoalID,
 							  queue_size=10)
 	listener_pose = tf.TransformListener()
+	seq_goal = 0
+	seq_initial = 0
 
 	with init_socket() as s:
 		while not rp.is_shutdown():
@@ -76,23 +87,26 @@ def main():
 
 				if data['name'] == 'goal_map':
 					message = PoseStamped()
-					create_pose_msg(data, message)
+					create_pose_msg(data, seq_goal, message)
 					message.header.frame_id = 'map'
 					pub_goal.publish(message)
+					seq_goal += 1
 				elif data['name'] == 'initial':
 					message = PoseWithCovarianceStamped()
-					create_pose_msg(data, message)
+					create_pose_msg(data, seq_initial, message)
 					message.header.frame_id = 'map'
 					message.pose.covariance = FIXED_COVARIANCE
 					pub_initial.publish(message)
+					seq_initial += 1
 				elif data['name'] == 'get_pose':
 					pose = read_recent_pose()
 					conn.sendall(pose)
 				elif data['name'] == 'goal_robot':
 					message = PoseStamped()
-					create_pose_msg(data, message)
+					create_pose_msg(data, seq_goal, message)
 					message.header.frame_id = 'base_link'
 					pub_goal.publish(message)
+					seq_goal += 1
 				elif data['name'] == 'cancel':
 					pub_cancel.publish(GoalID())
 
